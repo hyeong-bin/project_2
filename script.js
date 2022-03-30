bce4493fd785ac372b224477f9fee4e028a4929d
hyeong-bin <heyongbinjo@gmail.com>
2022-03-29 18:02:13 -0700

    revised index.html

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.querySelector('h1').innerText = "Quotes on Design"
+const designTitle = document.querySelector('.designtitle')
+designTitle.innerHTML = "A piece of advice for designers"
 
 const button = document.querySelector('button')
 const quote = document.querySelector('.quote')
 const byWho = document.querySelector('.who')
-const photo = document.querySelector('.photo')
 
 button.addEventListener('click', async () => {
     let response = await axios.get(
@@ -12,13 +12,23 @@ button.addEventListener('click', async () => {
     let designQuote = response.data[Math.floor(Math.random() * response.data.length)]
     quote.innerHTML = `${designQuote.content.rendered}`
     byWho.innerHTML = `${designQuote.title.rendered}`
-    let picResponse = await axios.get(`https://imsea.herokuapp.com//api/1?q=${byWho}`)
-    photo.innerHTML = `<img src=${picResponse}>`
 })
 
+const button2 = document.querySelector('.button2')
+const quote2 = document.querySelector('.quote2')
+// const byWho2 = document.querySelector('.who2')
+
+// button2.addEventListener('click', async () => {
+//     let slipResponse = await axios.get(
+//         `https://api.adviceslip.com/advice`
+//       )
+//     let slipQuote = slipResponse.data
+//     quote2.innerHTML = `${slipQuote.slip.advice}`
+//     // byWho2.innerHTML = `${slipQuote.title.rendered}`
+// })
 
 const myFunction = () => {
-    axios.get('https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand').then((response) => {
+    axios.get('https://api.adviceslip.com/advice').then((response) => {
         console.log(response)
       })
   }
